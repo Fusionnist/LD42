5182c94af1dd5209c7582cc9a72618e9c284e072
Louis Vassaux <louis.vassaux@sfr.fr>
2018-08-11 15:14:32 +0200

    dsq

---
+++ LD42/Class_Gameloop/Tile.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.FZT;
+using MonoGame.FZT.Assets;
+using MonoGame.FZT.Data;
+using MonoGame.FZT.Drawing;
+using MonoGame.FZT.Input;
+using MonoGame.FZT.Physics;
+using MonoGame.FZT.Sound;
+using MonoGame.FZT.UI;
+using MonoGame.FZT.XML;
+using System.Collections.Generic;
+
+namespace LD42
+{
+    class Tile : Entity
+    {
+        public Tile(DrawerCollection texes_, Vector2 pos_, List<Property> props_, string name_ = null): base(texes_, pos_, props_, name_, "tile")
+        {
+
+        }
+    }
+}
