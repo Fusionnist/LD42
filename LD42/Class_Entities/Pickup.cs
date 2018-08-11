05d864014e5cea0b864b133df7353a26eccb2d0f
Fusionnist <antoine.chartier@gmail.com>
2018-08-11 13:58:47 +0200

    classes

---
+++ LD42/Class_Entities/Pickup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MonoGame.FZT.Assets;
+using MonoGame.FZT.Drawing;
+
+using Microsoft.Xna.Framework;
+
+namespace LD42
+{
+    public class Pickup : Entity
+    {
+        public Pickup(DrawerCollection texes_, Vector2 pos_, List<Property> props_):base(texes_, pos_, props_)
+        {
+
+        }
+    }
+}
