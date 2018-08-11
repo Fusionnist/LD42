c07cc1e9cefb1096479f074c9965cd45906e727a
Fusionnist <antoine.chartier@gmail.com>
2018-08-11 14:10:15 +0200

    doot doot

--- LD42/Class_Entities/Item.cs
+++ LD42/Class_Entities/Item.cs
@@ -4,9 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using MonoGame.FZT.Assets;
+using MonoGame.FZT.Drawing;
+
+using Microsoft.Xna.Framework;
+
 namespace LD42
 {
-    class Item
+    public class Item : Entity
     {
+        public Item(DrawerCollection texes_, Vector2 pos_, List<Property> props_) : base(texes_, pos_, props_)
+        {
+
+        }
     }
 }
--- LD42/Class_Gameloop/Inventory.cs
+++ LD42/Class_Gameloop/Inventory.cs
@@ -4,11 +4,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using 
+using MonoGame.FZT.Assets;
+
+using Microsoft.Xna.Framework;
 
 namespace LD42
 {
     public class Inventory
     {
+        List<InventorySlot> slots;
+        int[,] taken;
+        int size;
+
+        public Inventory()
+        {
+
+        }
+
+        public void AddItem()
+        {
+
+        }
+
+        void AddSlot()
+        {
+
+        }
+
+        void AddSlotTo(Vector2 target)
+        {
+
+        }
+
+        public void Update(float es_)
+        {
+
+        }
+
+        void UpdatePool()
+        {
+
+        }
+
+        void CheckRecipes(List<Entity> pool)
+        {
+
+        }
+
+
     }
 }
