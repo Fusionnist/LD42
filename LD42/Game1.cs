fbf6b1608d5a8087d0765d5b588458c7cbf1dc73
Fusionnist <antoine.chartier@gmail.com>
2018-08-11 12:23:58 +0200

    gameloop

--- LD42/Game1.cs
+++ LD42/Game1.cs
@@ -2,27 +2,80 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
+using MonoGame.FZT;
+using MonoGame.FZT.Assets;
+using MonoGame.FZT.Data;
+using MonoGame.FZT.Drawing;
+using MonoGame.FZT.Input;
+using MonoGame.FZT.Physics;
+using MonoGame.FZT.Sound;
+using MonoGame.FZT.UI;
+using MonoGame.FZT.XML;
+
 namespace LD42
 {
     public class Game1 : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-        
+
+        Point vdims, wdims;
+        int windowDivider;
+        SceneCollection scenes;
+        InputProfile ipp;
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+            //GAME VALUES
+            windowDivider = 2;
+            vdims = new Point(200, 200);
+            wdims = new Point(1920 / windowDivider, 1080 / windowDivider);
+
+            graphics.PreferredBackBufferWidth = wdims.X;
+            graphics.PreferredBackBufferHeight = wdims.Y;
+            graphics.ApplyChanges();
         }
 
         protected override void Initialize()
         {
             base.Initialize();
+            //VALUES
+
+            //UTILITY
+            scenes = new SceneCollection();
+            scenes.scenes.Add(new Scene(
+                new RenderTarget2D(GraphicsDevice, vdims.X, vdims.Y),
+                new Rectangle(0,0,vdims.X, vdims.Y),
+                new Rectangle(0,0,wdims.X, wdims.Y),
+                "main"
+                ));
+
+            KeyManager[] keyManagers = new KeyManager[] { };
+            ipp = new InputProfile(keyManagers);
         }
 
         protected override void LoadContent()
         { 
-            spriteBatch = new SpriteBatch(GraphicsDevice);  
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            //LOAD XML
+
+            //LOAD TEXTURES
+
+            //LOAD SOUND
+
+            //LOAD ENTITIES
+
+            //LOAD UR MOM
+
+            //END - SETUP THE GAME!
+            SetupGame();
+        }
+
+        void SetupGame()
+        {
+
         }
 
         protected override void UnloadContent()
@@ -32,6 +85,13 @@ namespace LD42
 
         protected override void Update(GameTime gameTime)
         {
+            //GENERATE VALUES
+            float es = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            //UPDATE INPUT
+            ipp.Update(Keyboard.GetState(), GamePad.GetState(PlayerIndex.One));
+
+            //END
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
@@ -40,7 +100,22 @@ namespace LD42
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            //DRAW TO MAIN
+            scenes.SelectScene("main");
+            scenes.SetupScene(spriteBatch, GraphicsDevice);
+
+            GraphicsDevice.Clear(Color.Red);
+
+            spriteBatch.End();
+
+            //DRAW TO SCREEN
+            GraphicsDevice.SetRenderTarget(null);
+            spriteBatch.Begin();
+
+            scenes.DrawScene(spriteBatch, "main");
+
+            spriteBatch.End();
+
             base.Draw(gameTime);
         }
     }
