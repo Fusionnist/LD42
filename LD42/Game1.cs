beafa5a33200f0ef23ef62fab065389762b4e90f
Louis Vassaux <louis.vassaux@sfr.fr>
2018-08-11 13:31:16 +0200

    dqsdsq

--- LD42/Game1.cs
+++ LD42/Game1.cs
@@ -1,24 +1,37 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.FZT;
+using MonoGame.FZT.Data;
+using MonoGame.FZT.Drawing;
+using MonoGame.FZT.Input;
+using MonoGame.FZT.UI;
+using System.Collections.Generic;
 
 namespace LD42
 {
     public class Game1 : Game
     {
-        GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
+        CursorManager cursorManager;
         GameState gameState;
+        GraphicsDeviceManager graphics;
+        int currentUInb;
+        SpriteBatch spriteBatch;
+        UISystem[] uis;
+
         
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            gameState = GameState.Menu;
+            IsMouseVisible = true;
         }
 
         protected override void Initialize()
         {
+            gameState = GameState.Menu;
+            currentUInb = 0;
+
             base.Initialize();
         }
 
@@ -26,7 +39,16 @@ namespace LD42
 
         protected override void LoadContent()
         { 
-            spriteBatch = new SpriteBatch(GraphicsDevice);  
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            cursorManager = new CursorManager();
+            uis = new UISystem[]
+            {
+                new UISystem(new List<Button>()
+                {
+                    new Button("startGame", new Rectangle(0, 0, 800, 400), new TextureDrawer(Content.Load<Texture2D>("yesnpressed"), new TextureFrame(new Rectangle(0, 0, 800, 400), new Point(400, 200))), new TextureDrawer(Content.Load<Texture2D>("yesnpressed")))
+                }),
+                new UISystem()
+            };
         }
 
         protected override void UnloadContent()
@@ -39,7 +61,11 @@ namespace LD42
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            cursorManager.Update();
 
+            uis[currentUInb].HandleMouseInput(cursorManager);
+
+            HandleGameStateChanges();
 
 
 
@@ -49,7 +75,23 @@ namespace LD42
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+
+            uis[currentUInb].Draw(spriteBatch);
+
+            spriteBatch.End();
+
             base.Draw(gameTime);
+        }
+
+        protected void HandleGameStateChanges()
+        {
+            if (gameState == GameState.Menu && uis[currentUInb].IssuedCommand("startGame"))
+            {
+                gameState = GameState.Game;
+                currentUInb = 1;
+            }
         }
     }
 }
