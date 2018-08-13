ad76e553affc7cb0f41d3b533636afe693558534
Fusionnist <antoine.chartier@gmail.com>
2018-08-13 14:36:58 +0200

    a

--- LD42/Class_Entities/ShadowEnt.cs
+++ LD42/Class_Entities/ShadowEnt.cs
@@ -21,6 +21,7 @@ namespace LD42
 
         public override void Draw(SpriteBatch sb_, bool flipH_ = false, bool flipV_ = false, float angle_ = 0)
         {
+            if(!isDestroyed)
             textures.GetTex("shadow").Draw(sb_, new Vector2((int)pos.X, 62));
             base.Draw(sb_, flipH_, flipV_, angle_);
         }
